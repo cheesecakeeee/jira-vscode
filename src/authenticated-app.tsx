7b04bb26a32c961efd3101da96fd1c0d5bc1fe98
cheesecakeeee <eudoracheng@foxmail.com>
2024-05-20 12:44:17 +0800

    style: 用emotion变量的方式自定义可配置Row组件,并用Row组件布局

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,12 +1,36 @@
+import styled from "@emotion/styled";
+import { Row } from "components/lib";
 import { Button } from "antd";
 import { useAuth } from "context/auth-context";
 import { ProjectListScreen } from "screens/project-list";
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <Button onClick={logout}>退出登录</Button>
-      <ProjectListScreen></ProjectListScreen>
-    </div>
+    <Container>
+      <PageHeader between={true}>
+        <HeaderLeft gap={true}>
+          <h2>logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
+        </HeaderLeft>
+        <HeaderRight>
+          <Button onClick={logout}>退出登录</Button>
+        </HeaderRight>
+      </PageHeader>
+      <Main>
+        <ProjectListScreen></ProjectListScreen>
+      </Main>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`;
+const PageHeader = styled(Row)``;
+const HeaderLeft = styled(Row)``;
+const HeaderRight = styled.div``;
+
+const Main = styled.main``;
