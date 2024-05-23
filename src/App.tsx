fffcff7decf41d26c511c97a9e303e8abd81c374
cheesecakeeee <eudoracheng@foxmail.com>
2024-05-24 01:13:30 +0800

    feat: 自定义错误边界组件并应用到根组件中捕获渲染错误

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { UnauthenticatedApp } from "unauthenticated-app";
 import { AuthenticatedApp } from "authenticated-app";
 import { ConfigProvider } from "antd";
 import "./App.css";
+import { ErrorBoundary } from "components/error-boundary";
+import { FullPageErrorFallback } from "components/lib";
 
 function App() {
   const { user } = useAuth();
@@ -11,7 +13,9 @@ function App() {
     <ConfigProvider
       theme={{ token: { colorPrimary: "#0052cc", fontSize: 16 } }}
     >
-      <div>{user ? <AuthenticatedApp /> : <UnauthenticatedApp />}</div>
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}>
+        {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      </ErrorBoundary>
     </ConfigProvider>
   );
 }
---
+++ src/components/error-boundary.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+type FallbackRender = (props: { error: Error | null }) => React.ReactElement;
+
+export class ErrorBoundary extends React.Component<
+  React.PropsWithChildren<{ fallbackRender: FallbackRender }>,
+  { error: Error | null }
+> {
+  state = { error: null };
+
+  static getDerivedStateFromError(error: Error) {
+    // 更新状态，以便下一次渲染将显示后备 UI。
+    return { error };
+  }
+
+  render() {
+    const { error } = this.state;
+    const { fallbackRender, children } = this.props;
+    if (error) {
+      // 你可以渲染任何自定义后备 UI
+      return fallbackRender({ error });
+    }
+
+    return children;
+  }
+}
